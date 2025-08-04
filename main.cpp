7be4dca673237ea4d0124f4fa2ed7edfd45469f3
cyberduck1337 <cyberduck1337@proton.me>
2025-08-04 18:50:21 +0200

    updated main programm to demonstrate Gfx API

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-#include "fmt/base.h"
+#include "Gfx.hpp"
+#include <cstdint>
 
 int main(int argc, char** argv)
 {
-    fmt::println("Hello, World");
+    constexpr auto APPLICATION_NAME = "Emerald";
+    
+    constexpr uint32_t APPLICATION_DEFAULT_WINDOW_WIDHT = 1280;
+    constexpr uint32_t APPLICATION_DEFAULT_WINDOW_HEIGHT = 720;
+
+    Emerald::Gfx::initialize(APPLICATION_NAME, APPLICATION_DEFAULT_WINDOW_WIDHT, APPLICATION_DEFAULT_WINDOW_HEIGHT);
+    Emerald::Gfx::setClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    while (!Emerald::Gfx::windowShouldClose())
+    {
+        Emerald::Gfx::beginFrame();
+        Emerald::Gfx::endFrame();
+    }
+    Emerald::Gfx::destroy();
+    
     return 0;
 }
