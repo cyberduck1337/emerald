3858119d90727118c2c088dc68f4599d2ee62ea9
cyberduck1337 <cyberduck1337@proton.me>
2025-08-09 23:46:47 +0200

    main changed to meet Game arch

--- main.cpp
+++ main.cpp
@@ -1,26 +1,83 @@
-#include "Gfx.hpp"
+#include "Game.hpp"
+#include "Input.hpp"
 #include "Log.hpp"
-#include <cstdint>
+#include "Gfx.hpp"
+#include "System.hpp"
+#include "entt/entity/fwd.hpp"
+
+class FreeCameraComponent
+{
+public:
+    FreeCameraComponent(float speed, float sensetivity) : m_speed(speed), m_sensetivity(sensetivity)
+    {
+    }
+
+public:
+    float m_speed;
+    float m_sensetivity;
+};
+
+class FlyCameraSystem : public Emerald::ECS::System
+{
+public:
+    virtual void update(entt::registry& registry) override final
+    {
+        auto view = registry.view<Emerald::Gfx::Transform, Emerald::Gfx::Camera, const FreeCameraComponent>();
+        for(auto&& [entity, transform, camera, freeCameraComponent] : view.each())
+        {
+            Emerald::Log::debug("position: {} {} {}", transform.position.x, transform.position.y, transform.position.z);
+            if(Emerald::Input::getKeyDown(Emerald::Input::KeyCode::W))
+            {
+                transform.position += freeCameraComponent.m_speed * Emerald::Gfx::deltaTime() * transform.front();
+            }
+
+            if(Emerald::Input::getKeyDown(Emerald::Input::KeyCode::S))
+            {
+                transform.position -= freeCameraComponent.m_speed * Emerald::Gfx::deltaTime() * transform.front();
+            }
+
+            if(Emerald::Input::getKeyDown(Emerald::Input::KeyCode::A))
+            {
+                transform.position -= glm::normalize(glm::cross(transform.front(), transform.up())) * freeCameraComponent.m_speed * Emerald::Gfx::deltaTime();
+            }
+
+            if(Emerald::Input::getKeyDown(Emerald::Input::KeyCode::D))
+            {
+                transform.position += glm::normalize(glm::cross(transform.front(), transform.up())) * freeCameraComponent.m_speed * Emerald::Gfx::deltaTime();
+            }
+
+            if(Emerald::Input::getKeyDown(Emerald::Input::KeyCode::SPACE))
+            {
+                transform.position += freeCameraComponent.m_speed * Emerald::Gfx::deltaTime() * transform.up();
+            }
+
+            if(Emerald::Input::getKeyDown(Emerald::Input::KeyCode::LEFT_CONTROL))
+            {
+                transform.position -= freeCameraComponent.m_speed * Emerald::Gfx::deltaTime() * transform.up();
+            }
+
+            if(Emerald::Input::getMouseButtonDown(1))
+            {
+                const glm::vec2 mousePosition = Emerald::Input::getMousePosition();
+
+                float xoffset = (m_lastMousePosition.x - mousePosition.x) * freeCameraComponent.m_sensetivity;
+                float yoffset = (mousePosition.y - m_lastMousePosition.y) * freeCameraComponent.m_sensetivity;
+            }
+        }
+    }
+
+private:
+    glm::vec2 m_lastMousePosition;
+};
 
 int main(int argc, char** argv)
 {
-    constexpr auto APPLICATION_NAME = "Emerald";
-    
-    constexpr uint32_t APPLICATION_DEFAULT_WINDOW_WIDHT = 1280;
-    constexpr uint32_t APPLICATION_DEFAULT_WINDOW_HEIGHT = 720;
+    Emerald::Game game{};
+    entt::entity mainCamera = game.createEntity();
+    game.addComponent<Emerald::Gfx::Camera>(mainCamera, 45.0f, 0.1f, 100);
+    game.addComponent<FreeCameraComponent>(mainCamera, 1.0f, 0.1f);
+    game.addSystem<FlyCameraSystem>();
+    game.run();
 
-    Emerald::Log::initialize();
-    Emerald::Gfx::initialize(APPLICATION_NAME, APPLICATION_DEFAULT_WINDOW_WIDHT, APPLICATION_DEFAULT_WINDOW_HEIGHT);
-
-    Emerald::Gfx::setClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    while (!Emerald::Gfx::windowShouldClose())
-    {
-        Emerald::Gfx::beginFrame();
-        Emerald::Gfx::endFrame();
-    }
-
-    Emerald::Gfx::destroy();
-    Emerald::Log::destroy();
-    
     return 0;
 }
